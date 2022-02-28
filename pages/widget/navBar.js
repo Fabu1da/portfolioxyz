14434b5e943b826dca67a99030717a83af1830d2
Fabu1da <fabiusyy@gmail.com>
2022-02-28 13:24:54 +0100

    Update1

--- pages/widget/navBar.js
+++ pages/widget/navBar.js
@@ -1,25 +1,25 @@
 const navBar = () =>{
 return(
    
-      <div class="bg-black w-full fixed">
-        <div class="flex justify-between items-center px-20 mx-auto">
-            <div class="bg-gray-700 text-gray-100 text-4xl p-2">
+      <div className="bg-black w-full fixed">
+        <div className="flex justify-between items-center px-20 mx-auto">
+            <div className="bg-gray-700 text-gray-100 text-4xl p-2">
                 FK
             </div>
-            <div class=" hidden text-gray-100">
-                <a class="p-2"  href="">Home</a>
-                <a class="p-2"  href="">Feature</a>
-                <a class="p-2"  href="">Skills</a>
-                <a class="p-2"  href="">Work</a>
-                <a class="p-2"  href="">Contact</a>
+            <div className=" hidden text-gray-100">
+                <a className="p-2"  href="">Home</a>
+                <a className="p-2"  href="">Feature</a>
+                <a className="p-2"  href="">Skills</a>
+                <a className="p-2"  href="">Work</a>
+                <a className="p-2"  href="">Contact</a>
             </div>
-            <div class="text-gray-600 text-4xl">
-                <i class="fa-brands fa-instagram"></i>
-                <i class="fa-brands fa-facebook"></i>
-                <i class="fa-brands fa-twitter"></i>
+            <div className="text-gray-600 text-4xl">
+                <i className="fa-brands fa-instagram"></i>
+                <i className="fa-brands fa-facebook"></i>
+                <i className="fa-brands fa-twitter"></i>
             </div>
-            <div class="md:hidden text-gray-100 text-4xl">
-              <i class="fa-solid fa-bars"></i>
+            <div className="md:hidden text-gray-100 text-4xl">
+              <i className="fa-solid fa-bars"></i>
             </div>
         </div>
     </div>
