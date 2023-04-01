9ca69551726d3850d1b5a5fdf532a23b6a410616
Fabu1da <fabiusyy@gmail.com>
2023-04-01 21:10:05 +0200

    portfolio

--- pages/widget/contact.js
+++ pages/widget/contact.js
@@ -2,8 +2,11 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="flex justify-center bg-slate-800 gap-3 " id="contact">
-      <div className="flex gap-10 p-10">
+    <div className="bg-slate-800  p-8" id="contact">
+      <div className="text-center text-6xl text-white font-bold my-8">
+        Contact / Social Media
+      </div>
+      <div className="flex justify-center gap-10 p-10">
         <div className="p-10 shadow-xl rounded-2xl bg-slate-900 text-white">
           <h1 className=" text-xl text-white font-bold mb-5">
             Personal Contact
