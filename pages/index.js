d6218b8fba4af5f197aeb370eef88d68611f05a1
Fabu1da <fabiusyy@gmail.com>
2023-10-08 14:26:33 +0200

    new design

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,11 @@
 import NavBar from "./widget/navBar";
 // import Footer from "./widget/footer";
 import Project from "./components/project/project";
-
-import Image from "next/image";
-import hero from "../public/1.jpg";
-
 import Contact from "./widget/contact";
-import {
-  SiHtml5,
-  SiBootstrap,
-  SiCss3,
-  SiJavascript,
-  SiTailwindcss,
-  SiReact,
-  SiNextdotjs,
-} from "react-icons/si";
 import Technology from "./components/technology/technology";
 import About from "./components/aboutme/about";
 import Achievement from "./components/achivement/achievement";
+import Feature from "./components/feature/feature";
 // const cv_url = "https://localhost:3000/pdf/Fabien_Kavuganyi_cv.pdf";
 
 export default function Home() {
@@ -36,53 +24,7 @@ export default function Home() {
       <NavBar />
       <div className="main__bg font-bold pt-28 pb-8  " id="home">
         {/* containt */}
-        <div className="container  md:flex  justify-center items-center mx-auto relative overflow-hidden h-max  md:h-full ">
-          <div className=" flex justify-center  overflow-hidden md:px-14">
-            <div className="relative h-98 w-12  ">
-              <div className="absolute bottom-0 left-0 h-36 w-full bg-blue-600/[.06] rounded-bl-lg"></div>
-            </div>
-
-            <div className="relative overflow-hidden  rounded-tl-lg rounded-br-lg w-96  border-t-6 border-indigo-200 border-t-indigo-500">
-              <Image
-                src={hero}
-                alt=""
-                className=" p-0 scale-125 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-150  duration-300"
-              />
-            </div>
-            <div className="relative h-full w-12  ">
-              <div className="absolute top-0 right-0 h-24 w-full bg-green-600/[.06] rounded-tr-lg"></div>
-            </div>
-          </div>
-          <div className=" p-8 md:w-1/2 ">
-            <h1 className="text-4xl md:text-6xl text-white font-bold">
-              Fabien <span className="text-blue-600">Kavuganyi</span>
-            </h1>
-            <div className="my-4 under__title text-gray-200 text-2xl text-italic">
-              Frontend developer
-            </div>
-            <div className="transition ease-in-out duration-500 delay-150 text-white font-light border-l-8  border-gray-400 hover:border-l-8 pl-3 hover:border-blue-500 whitespace-normal">
-              Front end developer who focuses on writing clean, elegant and
-              effective code. Love HTML, CSS, NextJs, ReactJs and JavaScripts
-            </div>
-
-            <p className="my-5 text-gray-200 under__title">Main Technology </p>
-            <div className="flex text-white flex-wrap flex-row  justify-start flex-1 gap-3 mb-10 ">
-              <SiHtml5 style={{ fontSize: "50px", color: "#f06529" }} />
-              <SiCss3 style={{ fontSize: "50px", color: "#800000" }} />
-              <SiJavascript style={{ fontSize: "50px", color: "#F0DB4F" }} />
-              <SiBootstrap style={{ fontSize: "50px", color: "#e040fb" }} />
-              <SiTailwindcss style={{ fontSize: "50px", color: "#e3342f" }} />
-              <SiReact style={{ fontSize: "50px", color: "#61dbfb" }} />
-              <SiNextdotjs style={{ fontSize: "50px", color: "#45ce05" }} />
-            </div>
-            <button
-              onClick={handleDownload}
-              className="border-2 border-orange-800 px-10 py-5 rounded-full text-orange-800 hover:text-white hover:bg-orange-800 mt-4 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300"
-            >
-              Get My CV here
-            </button>
-          </div>
-        </div>
+        <Feature handleDownload={handleDownload} />
         <div className="md:ml-20 ml-5 h-[5px] w-[60px] bg-orange-600 mt-20  rounded-r-3xl" />
         <h1 className="headtext__base text-orange-600 md:ml-20 ml-5 my-3 text-semibold  text-[48px]">
           Project
--- pages/widget/navBar.js
+++ pages/widget/navBar.js
@@ -6,6 +6,12 @@ import {
   RiFacebookFill,
   RiInstagramFill,
   RiLinkedinFill,
+  RiHome2Line,
+  RiProjector2Line,
+  RiCodeLine,
+  RiUser2Line,
+  RiArchiveDrawerLine,
+  RiContactsLine,
 } from "react-icons/ri";
 const NavBar = () => {
   const [smallScreenNav, setSmallScreenNav] = useState(false);
@@ -13,74 +19,76 @@ const NavBar = () => {
     setSmallScreenNav((currentstate) => !currentstate);
   };
   return (
-    <div className="bg-black w-full z-10 fixed">
-      <div className="flex justify-between items-center md:px-20 mx-auto">
-        <div className="bg-gray-700 text-gray-100 text-4xl p-2">FK</div>
-        <div className=" hidden  md:block text-gray-100">
-          <a className="p-2" href="#home">
-            Home
-          </a>
+    <div className=" h-full   flex mx-2 content-center items-center z-10 fixed">
+      <div className="bg-black md:h-[80%]  md:rounded-full shadow-md shadow-gray-600 flex content-between items-center z-10 fixed">
+        <div className="flex flex-col justify-between items-center  mx-auto">
+          <div className="bg-gray-700 text-gray-100 text-4xl p-2">FK</div>
+          <div className=" flex flex-1 flex-col text-4xl   text-gray-100">
+            <a className="p-2 " href="#home">
+              <RiHome2Line />
+            </a>
 
-          <a className="p-2" href="#project">
-            Projects
-          </a>
-          <a className="p-2" href="#technology">
-            Technology
-          </a>
-          <a className="p-2" href="#about">
-            About Me
-          </a>
-          <a className="p-2" href="#achievement">
-            Achievement
-          </a>
-          <a className="p-2" href="#contact">
-            Contact
-          </a>
+            <a className="p-2" href="#project">
+              <RiProjector2Line />
+            </a>
+            <a className="p-2" href="#technology">
+              <RiCodeLine />
+            </a>
+            <a className="p-2" href="#about">
+              <RiUser2Line />
+            </a>
+            <a className="p-2" href="#achievement">
+              <RiArchiveDrawerLine />
+            </a>
+            <a className="p-2" href="#contact">
+              <RiContactsLine />
+            </a>
+          </div>
+          <div className="text-gray-600  text-4xl flex flex-col gap-3">
+            <a href="https://www.instagram.com/fabu1da/">
+              <RiInstagramFill />
+            </a>
+            <a href="https://www.facebook.com/kavuganyi.fabius/">
+              <RiFacebookFill />
+            </a>
+            <a href="https://www.linkedin.com/in/kavuganyi-fabien-717a46202/">
+              <RiLinkedinFill />
+            </a>
+            <a href="https://github.com/Fabu1da">
+              <RiGithubFill />
+            </a>
+          </div>
+          <div className="md:hidden text-gray-100 text-4xl mr-2">
+            {smallScreenNav ? (
+              <RiCloseLine onClick={usehandleNavigation} />
+            ) : (
+              <RiMenu3Line onClick={usehandleNavigation} />
+            )}
+          </div>
         </div>
-        <div className="text-gray-600 text-4xl flex gap-3">
-          <a href="https://www.instagram.com/fabu1da/">
-            <RiInstagramFill />
-          </a>
-          <a href="https://www.facebook.com/kavuganyi.fabius/">
-            <RiFacebookFill />
-          </a>
-          <a href="https://www.linkedin.com/in/kavuganyi-fabien-717a46202/">
-            <RiLinkedinFill />
-          </a>
-          <a href="https://github.com/Fabu1da">
-            <RiGithubFill />
-          </a>
-        </div>
-        <div className="md:hidden text-gray-100 text-4xl mr-2">
-          {smallScreenNav ? (
-            <RiCloseLine onClick={usehandleNavigation} />
-          ) : (
-            <RiMenu3Line onClick={usehandleNavigation} />
-          )}
-        </div>
-      </div>
-      {smallScreenNav && (
-        <div className="text-white flex flex-col transition duration-150 ease-in-out">
-          <a className="p-4 border-b border-b-gray-800" href="#home">
-            Home
-          </a>
-          {/* <a className="p-2" href="">
+        {smallScreenNav && (
+          <div className="text-white flex flex-col transition duration-150 ease-in-out">
+            <a className="p-4 border-b border-b-gray-800" href="#home">
+              Home
+            </a>
+            {/* <a className="p-2" href="">
             Feature
           </a> */}
-          <a className="p-4 border-b border-b-gray-800" href="#project">
-            Projects
-          </a>
-          <a className="p-4 border-b border-b-gray-800" href="#technology">
-            Technology
-          </a>
-          <a className="p-4 border-b border-b-gray-800" href="#achievement">
-            Achievement
-          </a>
-          <a className="p-4" href="#contact">
-            Contact
-          </a>
-        </div>
-      )}
+            <a className="p-4 border-b border-b-gray-800" href="#project">
+              Projects
+            </a>
+            <a className="p-4 border-b border-b-gray-800" href="#technology">
+              Technology
+            </a>
+            <a className="p-4 border-b border-b-gray-800" href="#achievement">
+              Achievement
+            </a>
+            <a className="p-4" href="#contact">
+              Contact
+            </a>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
